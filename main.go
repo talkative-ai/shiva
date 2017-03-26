7e62890a23839601a19cebaf8b805e0e011b89b9
Wyatt Arent <hello@wyatt.engineer>
2017-03-26 10:44:08 -0700

    Prepare mux router

--- main.go
+++ main.go
@@ -3,15 +3,33 @@ package prospectacle
 import (
 	"io"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func init() {
-	http.HandleFunc("/.well-known/acme-challenge/NX1lFw-WUrXPNfjUPvgEmzulsrpsobWGa3SW_GO5MPQ", root)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/.well-known/acme-challenge/NX1lFw-WUrXPNfjUPvgEmzulsrpsobWGa3SW_GO5MPQ", ssl)
+
+	http.Handle("/", r)
+}
+
+func home(w http.ResponseWriter, rq *http.Request) {
+	io.WriteString(w, "Hello World!")
 }
 
 // [START func_root]
-func root(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, rq *http.Request) {
+
 	io.WriteString(w, "NX1lFw-WUrXPNfjUPvgEmzulsrpsobWGa3SW_GO5MPQ.dHP9qyK89IfuTqoierLcfa3E_gZetc7DP7B5SuAznMk")
 }
 
 // [END func_root]
+
+func ssl(w http.ResponseWriter, rq *http.Request) {
+
+	io.WriteString(w, "NX1lFw-WUrXPNfjUPvgEmzulsrpsobWGa3SW_GO5MPQ.dHP9qyK89IfuTqoierLcfa3E_gZetc7DP7B5SuAznMk")
+}
