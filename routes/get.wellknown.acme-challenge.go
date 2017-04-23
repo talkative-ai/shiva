3d1d61cdcf3194fcea43157bce1c5183a0dbdf5c
Wyatt Arent <hello@wyatt.engineer>
2017-04-23 15:43:17 -0700

    Add ssl

---
+++ routes/get.wellknown.acme-challenge.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/phrhero/stdapi/router"
+
+	"github.com/phrhero/stdapi/prehandle"
+)
+
+// GetWellknownAcmeChallenge router.Route
+// Method: "GET"
+// SSL Verification method -- Must be updated every 3 months
+// Last update: April 23, 2017
+var GetWellknownAcmeChallenge = &router.Route{
+	Path:       "/.well-known/acme-challenge/_wt6Wp8oG8DIGCHDCB0JKRl0UFbxLUwydnGT4WAyuf0",
+	Method:     "GET",
+	Handler:    http.HandlerFunc(GetWellknownAcmeChallengeHandler),
+	Prehandler: []prehandle.Prehandler{},
+}
+
+func GetWellknownAcmeChallengeHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "_wt6Wp8oG8DIGCHDCB0JKRl0UFbxLUwydnGT4WAyuf0.eAqIP9l15IZFgWuMXfi_L1lwiD-53l7pz_q1ENaRX_Q")
+}
