e977a15c76990444af5190d9d6f7bd1971b67225
Wyatt Arent <hello@wyatt.engineer>
2017-09-01 16:30:02 -0700

    Update Sync postgres Update project get

--- routes/get.project.go
+++ routes/get.project.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"strconv"
 
+	"github.com/artificial-universe-maker/go-utilities/db"
+	"github.com/artificial-universe-maker/go-utilities/models"
 	"github.com/artificial-universe-maker/go-utilities/myerrors"
 	"github.com/artificial-universe-maker/go-utilities/prehandle"
 	"github.com/artificial-universe-maker/go-utilities/router"
@@ -30,14 +32,29 @@ func getProjectHandler(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.ParseInt(urlparams["id"], 10, 64)
 	if err != nil {
-		myerrors.ServerError(w, r, fmt.Errorf("%v+", urlparams))
+		myerrors.Respond(w, &myerrors.MySimpleError{
+			Code:    http.StatusBadRequest,
+			Message: "bad_id",
+			Req:     r,
+		})
 		return
 	}
 
-	// Validate token
-
 	// Validate project access
+	project := &models.AumProject{}
+	err = db.DBMap.SelectOne(project, "SELECT * FROM workbench_projects WHERE id=$1", id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	member := &models.TeamMember{}
+	err = db.DBMap.SelectOne(member, `SELECT t."Role" FROM workbench_projects AS p JOIN team_members AS t ON t."TeamID"=p."TeamID" AND t."UserID"=1 WHERE p."ID"=1`)
+	if member.TeamID != 1 || err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
 	// Return project data
-
+	json.NewEncoder(w).Encode(project)
 }
