af0be4143737431bfeb4dbac9fcdcd7d83fcd806
Wyatt Arent <hello@wyatt.engineer>
2017-10-13 22:50:56 -0700

    Update project creation Add project limit Validate project doesn't exist Create project

--- routes/post.project.go
+++ routes/post.project.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strings"
 
+	utilities "github.com/artificial-universe-maker/go-utilities"
 	"github.com/artificial-universe-maker/go-utilities/db"
 	"github.com/artificial-universe-maker/go-utilities/models"
 	"github.com/artificial-universe-maker/go-utilities/myerrors"
@@ -30,20 +32,78 @@ type postProjectRequest struct {
 
 func postProjectHandler(w http.ResponseWriter, r *http.Request) {
 
+	token, err := utilities.ParseJTWClaims(w.Header().Get("x-token"))
+	tknData := token["data"].(map[string]interface{})
+
 	project := new(models.AumProject)
 	postProject := postProjectRequest{}
 
-	err := json.Unmarshal([]byte(r.Header.Get("X-Body")), postProject)
+	err = json.Unmarshal([]byte(r.Header.Get("X-Body")), &postProject)
 	if err != nil {
 		myerrors.ServerError(w, r, err)
 		return
 	}
 
-	project.Title = postProject.Title
+	var count int
 
-	err = db.DBMap.Insert(project)
+	res, err := db.Instance.Query(`
+		SELECT COUNT(p."ID") as "ProjectCount"
+		FROM workbench_projects p
+		JOIN team_members as m ON m."UserID"=$1
+		JOIN teams as t ON t."ID"=m."TeamID"
+		WHERE p."TeamID"=t."ID"
+	`, tknData["user_id"])
 	if err != nil {
-		fmt.Println(err)
+		myerrors.ServerError(w, r, err)
+		return
+	}
+
+	team := models.TeamMember{}
+
+	err = db.DBMap.SelectOne(&team, `SELECT "TeamID" FROM team_members WHERE "UserID"=$1`, tknData["user_id"])
+	if err != nil {
+		myerrors.ServerError(w, r, err)
+		return
+	}
+
+	for res.Next() {
+		err = res.Scan(&count)
+		if err != nil {
+			myerrors.ServerError(w, r, err)
+			return
+		}
+	}
+
+	if count >= db.GetMaxProjects() {
+		myerrors.Respond(w, &myerrors.MySimpleError{
+			Code:    http.StatusForbidden,
+			Message: "project_limit_reached",
+			Req:     r,
+		})
+		return
+	}
+
+	project.Title = postProject.Title
+	postProject.Title = ""
+
+	err = db.DBMap.SelectOne(&postProject, `SELECT * FROM workbench_projects WHERE Lower("Title")=Lower($1)`, project.Title)
+
+	if err != sql.ErrNoRows && strings.ToLower(postProject.Title) == strings.ToLower(project.Title) {
+		myerrors.Respond(w, &myerrors.MySimpleError{
+			Code:    http.StatusConflict,
+			Message: "project_exists",
+			Req:     r,
+		})
+		return
+	} else if err != sql.ErrNoRows && err != nil {
+		myerrors.ServerError(w, r, err)
+		return
+	}
+
+	_, err = db.Instance.Exec(`INSERT INTO workbench_projects ("Title", "TeamID") VALUES ($1, $2) RETURNING "ID"`, project.Title, team.TeamID)
+	if err != nil {
+		myerrors.ServerError(w, r, err)
+		return
 	}
 
 	return
